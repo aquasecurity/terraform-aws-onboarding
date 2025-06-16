c01b39601232f8332313c390b5a16ae1b5021660
Noam Shraga <noam.shraga@aquasec.com>
2025-06-16 12:18:33 +0300

    SAAS-29475 - Auto-Discovery | AWS | Organization | Terraform | Split volume scanning deployment
    Resolves: SAAS-29475

--- main.tf
+++ main.tf
@@ -70,4 +70,5 @@ module "organization" {
   custom_vpc_subnet_route_table1_name = var.custom_vpc_subnet_route_table1_name
   custom_vpc_subnet_route_table2_name = var.custom_vpc_subnet_route_table2_name
   custom_cspm_regions                 = var.custom_cspm_regions
+   volume_scanning_deployment         = var.volume_scanning_deployment
 }
